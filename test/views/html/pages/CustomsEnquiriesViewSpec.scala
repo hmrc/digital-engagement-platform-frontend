a63894f7fa879d07ecd5a0e72a5bad767ccfd92a
digital032584 <tonymyers@hotmail.com>
2021-01-06 16:54:17 +0000

    DEP-1268: update webchat Customs page opening times

--- test/views/html/pages/CustomsEnquiriesViewSpec.scala
+++ test/views/html/pages/CustomsEnquiriesViewSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,8 +37,9 @@ class CustomsEnquiriesViewSpec extends ChatViewBehaviours {
       returnUrl,
       Seq(
         "Opening times:",
-        "Monday to Friday, 8am to 6pm",
-        "Closed weekends and bank holidays."
+        "Monday to Friday, 8am to 8pm",
+        "Saturday, 8am to 4pm",
+        "Closed Sundays and bank holidays."
       )
     )
   }
