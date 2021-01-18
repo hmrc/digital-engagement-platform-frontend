1f352ec9f5b5f336c81c38e6414e056d68a34793
Jay Nabonne <jaynabonne@gmail.com>
2021-01-18 15:48:22 +0000

    DEP-1344: Remove higher level behaviour from lower level code. Allows for different behaviour with CUI. Incomplete tests.

--- app/assets/javascripts/waitForChanges.js
+++ app/assets/javascripts/waitForChanges.js
@@ -2,6 +2,18 @@ import * as elementWatcher from './waitForEl'
 import * as statusObserver from './statusObserver'
 import * as dataLayerUpdater from './updateDatalayer'
 
+export function waitForCUI(d, w) {
+  $(window).on("load", function () {
+      elementWatcher.waitForEl('#nuanMessagingFrame', function () {
+
+        dataLayerUpdater.updateDataLayer(el,w,d);
+        statusObserver.observeStatus(el,w,d);
+      },
+      function() {
+        console.log("Timed out waiting for CUI chat to appear");
+      });
+  });
+};
 
 export function waitForChanges(d, w) {
   $(window).on("load", function () {
@@ -17,9 +29,18 @@ export function waitForChanges(d, w) {
 };
 
 function waitForNuanceElement(el,w,d) {
-  elementWatcher.waitForEl(el, function () {
-    dataLayerUpdater.updateDataLayer(el,w,d);
+  elementWatcher.waitForEl(el,
+    function () {
+      updateDataLayer(el,w,d);
 
-    statusObserver.observeStatus(el,w,d);
-  },w);
+      observeStatus(el,w,d);
+    },
+    function() {
+      const assistantUsed = w.location.pathname.includes("virtual-assistant")
+        ? "The digital assistant"
+        : "Webchat";
+      $(el).text(assistantUsed + ' is experiencing technical difficulties. Please keep refreshing the page to try again.')
+      reportEvent(w,createDataLayerElement(availabilities.NuanceUnavailable, el))
+    }
+  );
 }
