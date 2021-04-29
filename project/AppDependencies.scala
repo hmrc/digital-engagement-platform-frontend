4a11a3c4badf1949ccc2d38e1557a15f4a24deca
Jay Nabonne <jaynabonne@gmail.com>
2021-04-29 13:47:53 +0100

    DEP-1766: Upgrade to Play 2.8 and latest libraries (#244)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,21 +7,19 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "org.webjars.npm" % "hmrc-frontend" % "1.15.1",
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.3.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.49.0-play-27",
-    "uk.gov.hmrc" %% "url-builder" % "3.4.0-play-27",
-    "uk.gov.hmrc" %% "digital-engagement-platform-chat" % "0.14.0-play-27",
-    "uk.gov.hmrc" %% "http-verbs-play-27" % "12.3.0"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "4.3.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.59.0-play-28",
+    "uk.gov.hmrc" %% "url-builder" % "3.5.0-play-28",
+    "uk.gov.hmrc" %% "digital-engagement-platform-chat" % "0.16.0-play-28"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.3.0" % "test",
     "org.scalatest" %% "scalatest" % "3.0.8" % "test",
     "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
     "org.jsoup" % "jsoup" % "1.10.2" % "test",
     "org.mockito" % "mockito-all" % "1.10.19" % "test",
     "org.pegdown" % "pegdown" % "1.6.0" % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % "test"
+    "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % "test"
   )
 
   val all: Seq[ModuleID] = compile ++ test
