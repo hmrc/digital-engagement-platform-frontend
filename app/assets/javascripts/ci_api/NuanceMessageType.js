a8576a17439b44d41ee8c86649e2136a790909a0
Jay Nabonne <jaynabonne@gmail.com>
2021-07-30 14:55:28 +0100

    DEP-2016: Handle transfer to agent, agent joined room, and agent left room.

--- app/assets/javascripts/ci_api/NuanceMessageType.js
+++ app/assets/javascripts/ci_api/NuanceMessageType.js
@@ -4,3 +4,6 @@ export const Chat_Communication = 'chat.communication';
 export const Chat_AutomationRequest = 'chat.automaton_request';
 export const Chat_CommunicationQueue = 'chat.communication.queue';
 export const Chat_Denied = 'chat.denied';
+export const Chat_Exit = 'chat.exit';
+export const Chat_TransferResponse = "chat.transfer_response";
+export const ChatRoom_MemberConnected = "chatroom.member_connected";
--- test/javascripts/ci_api/ChatStates.spec.js
+++ test/javascripts/ci_api/ChatStates.spec.js
@@ -205,5 +205,91 @@ describe("Chat States", () => {
             expect(container.transcript.addAutomatonMsg).toHaveBeenCalledWith("Beep boop. I am a robot.");
             expect(container.transcript.addCustomerMsg).toHaveBeenCalledWith("Hello to you");
         });
+
+        it("sends TransferResponse to the transcript", () => {
+            const [sdk, container] = createEngagedStateDependencies();
+
+            const state = new ChatStates.EngagedState(sdk, container, []);
+
+            const handleMessage = sdk.getMessages.mock.calls[0][0];
+            const message = {
+                data:  {
+                    "ltime": "1651712",
+                    "state": "transfer",
+                    "reason": "Transfer from Virtual Assistant [HMRC] to agent JayNabonne",
+                    "status": "accepted",
+                    "messageType": "chat.transfer_response",
+                    "engagementID": "388260662637696059",
+                    "messageTimestamp": "1627648283000",
+                    "client.display.text": "I'm connecting you to the next available webchat adviser.",
+                    "msg.originalrequest.id": "385445912674772418"
+                }
+            };
+
+            handleMessage(message);
+            expect(container.transcript.addSystemMsg).toHaveBeenCalledWith("I'm connecting you to the next available webchat adviser.");
+        });
+
+        it("sends MemberConnected to the transcript", () => {
+            const [sdk, container] = createEngagedStateDependencies();
+
+            const state = new ChatStates.EngagedState(sdk, container, []);
+
+            const handleMessage = sdk.getMessages.mock.calls[0][0];
+            const message = {
+                data:  {
+                    "owner": "true",
+                    "tc.mode": "transfer",
+                    "agent.alias": "Jay",
+                    "messageType": "chatroom.member_connected",
+                    "agentGroupID": "10006721",
+                    "display.text": "Agent enters chat (as Jay)",
+                    "engagementID": "388260662637696059",
+                    "business_unit.id": "19001214",
+                    "config.script_id": "12201319",
+                    "messageTimestamp": "1627648283000",
+                    "chatroom.member.id": "42391918",
+                    "client.display.text": "You're now talking to Jay",
+                    "chatroom.member.name": "JayNabonne",
+                    "chatroom.member.type": "agent"
+                }
+            };
+
+            handleMessage(message);
+            expect(container.transcript.addSystemMsg).toHaveBeenCalledWith("You're now talking to Jay");
+        });
+
+        it("reports chat exit in transcript", () => {
+            const [sdk, container] = createEngagedStateDependencies();
+
+            const state = new ChatStates.EngagedState(sdk, container, []);
+
+            const handleMessage = sdk.getMessages.mock.calls[0][0];
+            const message = {
+                data: {
+                    "state": "closed",
+                    "agentID": "42391918",
+                    "sessionId": "2493130538282329498",
+                    "user.type": "agent",
+                    "aeapi.mode": "true",
+                    "disp.notes": "",
+                    "messageType": "chat.exit",
+                    "display.text": "Agent 'Jay' exits chat",
+                    "engagementID": "388260662810973280",
+                    "disp.reason.0": "No answer given by customer or Not asked as chat terminated",
+                    "disp.category.0": "Enquiry Handled - Customer Question",
+                    "external_user.ip": "80.0.102.28",
+                    "messageTimestamp": "1627651338000",
+                    "config.session_id": "2493130538282329498",
+                    "disposition.answer": "19005454:25243342",
+                    "conversation_resolved": "false",
+                    "msg.originalrequest.id": "2493130538484377173",
+                    "auto_transfer_ignored_chatroom": "false"
+                }
+            };
+
+            handleMessage(message);
+            expect(container.transcript.addSystemMsg).toHaveBeenCalledWith("Agent 'Jay' exits chat");
+        });
     });
 });
