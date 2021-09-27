4b009acdb30ac405554b630d24e7216770060ea5
lmugnai <11977337+lmugnai@users.noreply.github.com>
2021-09-27 10:31:50 +0100

    further fixes to post chat survey events

--- app/assets/javascripts/ci_api/PostChatSurveyService.js
+++ app/assets/javascripts/ci_api/PostChatSurveyService.js
@@ -51,12 +51,13 @@ export default class PostChatSurveyService {
             preAssigned: this.sdk.isConnected() && !chatParams.agentID,
             automatonID: automaton.id,
             unique_node_id: "node_1",
-            "custom.decisiontree.nodeID": "HMRC_PostChat_Guidance - Initial",
+            "custom.decisiontree.nodeID": encodeURI("HMRC_PostChat_Guidance - Initial"),
             automatonAttributes: "",
             visitorAttributes: chatParams.getVisitorAttributes(),
             clientID: Number(chatParams.siteID),
             businessUnitID: chatParams.businessUnitID,
             businessRuleID: Number(chatParams.brID),
+            busUnitID: chatParams.businessUnitID,
             BRName: chatParams.chatTitle,
             agentGroupID: chatParams.agId,
             availableAgentAttributes: chatParams.agentAttributes,
@@ -68,47 +69,11 @@ export default class PostChatSurveyService {
             browserType: chatParams.browserType,
             browserVersion: chatParams.browserVersion,
             surveyId: Number(survey.id),
-            "custom.decisiontree.questionIDs": "0." + survey.questions[0].id + ",1." + survey.questions[1].id + ",2." + survey.questions[2].id,
-            "custom.decisiontree.questions": "0." + survey.questions[0].text + ",1." + survey.questions[1].text + ",2." + survey.questions[2].text,
-            "custom.decisiontree.answerIDs":"0." + survey.answers[0].id + ",1." + survey.answers[1].id + ",2." + survey.answers[2].id,
-            "custom.decisiontree.answers": "0." + survey.answers[0].text + ",1." + survey.answers[1].text + ",2." + survey.answers[2].text,
-            "custom.decisiontree.answerTypes": "0,0,0",
-            "custom.decisiontree.answersNumeric": "0,1,2",
-            clientTimestamp: timestamp
-        };
-
-        const contentSentToCustomerEvent = {
-            _domain: "automaton",
-            evt: "contentSentToCustomer",
-            automatonType: "satisfactionSurvey",
-            siteID: Number(chatParams.siteID),
-            customerID: chatParams.thisCustomerID,
-            incAssignmentID: chatParams.sessionID,
-            pageID: Number(chatParams.launchPageId),
-            sessionID: chatParams.sessionID,
-            chatID: chatParams.chatID,
-            agentID: chatParams.agentID,
-            preAssigned: this.sdk.isConnected() && !chatParams.agentID,
-            automatonID: automaton.id,
-            visitorAttributes: chatParams.getVisitorAttributes(),
-            businessUnitID: chatParams.businessUnitID,
-            businessRuleID: Number(chatParams.brID),
-            BRName: chatParams.chatTitle,
-            agentGroupID: chatParams.agId,
-            availableAgentAttributes: chatParams.agentAttributes,
-            brAttributes: chatParams.ruleAttributes,
-            countryCode: chatParams.countryCode,
-            regionCode: chatParams.regionCode,
-            deviceType: chatParams.deviceType,
-            operatingSystemType: chatParams.operatingSystemType,
-            browserType: chatParams.browserType,
-            browserVersion: chatParams.browserVersion,
-            surveyId: Number(survey.id),
-            unique_node_id: "node_1",
-            "custom.decisiontree.nodeID": "HMRC_PostChat_Guidance - Thank You",
-            automatonAttributes: "node_id,node_1;node_name,HMRC_PostChat_Guidance - Thank You;node_type,thankyou;node_is_outcome,1",
-            "custom.decisiontree.questions": "Thank you",
-            "custom.decisiontree.questionIDs": "node_1",
+            "custom.decisiontree.questionIDs": encodeURI("0." + survey.questions[0].id + ",1." + survey.questions[1].id + ",2." + survey.questions[2].id),
+            "custom.decisiontree.questions": encodeURI("0." + survey.questions[0].text + ",1." + survey.questions[1].text + ",2." + survey.questions[2].text),
+            "custom.decisiontree.answerIDs": encodeURI("0." + survey.answers[0].id + ",1." + survey.answers[1].id + ",2." + survey.answers[2].id),
+            "custom.decisiontree.answers": encodeURI("0." + survey.answers[0].text + ",1." + survey.answers[1].text + ",2." + survey.answers[2].text),
+            "custom.decisiontree.answerTypes": encodeURI("0,0,0"),
             clientTimestamp: timestamp
         };
 
@@ -132,6 +97,7 @@ export default class PostChatSurveyService {
             clientID: Number(chatParams.siteID),
             businessUnitID: chatParams.businessUnitID,
             businessRuleID: Number(chatParams.brID),
+            busUnitID: chatParams.businessUnitID,
             BRName: chatParams.chatTitle,
             agentGroupID: chatParams.agId,
             availableAgentAttributes: chatParams.agentAttributes,
@@ -151,7 +117,7 @@ export default class PostChatSurveyService {
         console.log("===== submitPostChatSurvey =====");
 
         try {
-            this.sdk.logEventToDW({eventList:[customerRespondedEvent, contentSentToCustomerEvent, endedEvent]});
+            this.sdk.logEventToDW({eventList:[customerRespondedEvent, endedEvent]});
         } catch (e) {
             console.error("!!!! logEventToDW got exception: ", e);
         }
@@ -164,7 +130,7 @@ export default class PostChatSurveyService {
             _domain: "automaton",
             evt: "started",
             automatonType: "satisfactionSurvey",
-            automatonStartedBy: "survey, survey",
+            automatonStartedBy: "survey,survey",
             startedIn: "chat",
             type: "satisfactionSurvey",
             clientTimestamp: timestamp,
@@ -202,9 +168,9 @@ export default class PostChatSurveyService {
             _domain: "automaton",
             evt: "contentSentToCustomer",
             unique_node_id: "node_1",
-            "custom.decisiontree.nodeID": "HMRC_PostChat_Guidance - Initial",
-            "custom.decisiontree.questions": "0." + survey.questions[0].text + ",1." + survey.questions[1].text + ",2." + survey.questions[2].text,
-            "custom.decisiontree.questionIDs": "0." + survey.questions[0].id + ",1." + survey.questions[1].id + ",2." + survey.questions[2].id,
+            "custom.decisiontree.nodeID": encodeURI("HMRC_PostChat_Guidance - Initial"),
+            "custom.decisiontree.questions": encodeURI("0." + survey.questions[0].text + ",1." + survey.questions[1].text + ",2." + survey.questions[2].text),
+            "custom.decisiontree.questionIDs": encodeURI("0." + survey.questions[0].id + ",1." + survey.questions[1].id + ",2." + survey.questions[2].id),
             clientTimestamp: timestamp,
             automatonType: "satisfactionSurvey",
             chatID: chatParams.chatID,
