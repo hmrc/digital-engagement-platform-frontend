456768597bb7b9a2178ebfbdc9bca31a744ac321
Lorenzo Mugnai <11977337+lmugnai@users.noreply.github.com>
2021-11-23 12:04:03 +0000

    Update Chat Library (#336)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-28"       % "5.16.0",
     "uk.gov.hmrc"     %% "play-frontend-hmrc"               % "0.94.0-play-28",
     "uk.gov.hmrc"     %% "url-builder"                      % "3.5.0-play-28",
-    "uk.gov.hmrc"     %% "digital-engagement-platform-chat" % "0.22.0-play-28"
+    "uk.gov.hmrc"     %% "digital-engagement-platform-chat" % "0.23.0-play-28"
   )
 
   val test: Seq[ModuleID] = Seq(
