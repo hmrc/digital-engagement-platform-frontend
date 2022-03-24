4a174a53bca8ef7605119eb07e89f86df796dfc7
pratikrana06 <63359242+pratikrana06@users.noreply.github.com>
2022-03-24 16:32:34 +0000

    [DEP-2403][PR]:Upgraded play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "org.webjars.npm" %  "hmrc-frontend"                    % "1.35.2",
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-28"       % "5.21.0",
-    "uk.gov.hmrc"     %% "play-frontend-hmrc"               % "0.94.0-play-28",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc"               % "3.7.0-play-28",
     "uk.gov.hmrc"     %% "url-builder"                      % "3.6.0-play-28",
     "uk.gov.hmrc"     %% "digital-engagement-platform-chat" % "0.26.0-play-28"
   )
@@ -16,8 +16,8 @@ object AppDependencies {
     "org.jsoup"               %  "jsoup"              % "1.14.3"  % "test",
     "org.mockito"             %  "mockito-all"        % "1.10.19" % "test",
     "org.pegdown"             %  "pegdown"            % "1.6.0"   % "test",
-    "org.scalatestplus.play"  %% "scalatestplus-play" % "5.0.0"   % "test",
-    "com.vladsch.flexmark"    % "flexmark-all"        % "0.35.10" % "test"
+    "org.scalatestplus.play"  %% "scalatestplus-play" % "5.1.0"   % "test",
+    "com.vladsch.flexmark"    %  "flexmark-all"       % "0.35.10" % "test"
   )
 
   val all: Seq[ModuleID] = compile ++ test
