98d710586e9040bac3cc42e3c4f98e62130ff551
pratikrana06 <63359242+pratikrana06@users.noreply.github.com>
2022-03-25 16:54:48 +0000

    [DEP-2403][PR]:Upgraded play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "org.webjars.npm" %  "hmrc-frontend"                    % "1.35.2",
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-28"       % "5.21.0",
-    "uk.gov.hmrc"     %% "play-frontend-hmrc"               % "3.7.0-play-28",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc"               % "3.8.0-play-28",
     "uk.gov.hmrc"     %% "url-builder"                      % "3.6.0-play-28",
     "uk.gov.hmrc"     %% "digital-engagement-platform-chat" % "0.26.0-play-28"
   )
