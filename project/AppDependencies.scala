e302f786e452d9965ccbc8785cd4546540ef343c
Pratik Rana <63359242+pratikrana06@users.noreply.github.com>
2022-03-29 08:27:05 +0100

    [DEP-2403][PR]:Scalatest upgrades (#385)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,13 +11,13 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"          % "3.2.3"   % "test",
+    "org.scalatest"           %% "scalatest"          % "3.2.11"  % "test",
     "org.scalacheck"          %% "scalacheck"         % "1.15.4"  % "test",
     "org.jsoup"               %  "jsoup"              % "1.14.3"  % "test",
     "org.mockito"             %  "mockito-all"        % "1.10.19" % "test",
     "org.pegdown"             %  "pegdown"            % "1.6.0"   % "test",
     "org.scalatestplus.play"  %% "scalatestplus-play" % "5.1.0"   % "test",
-    "com.vladsch.flexmark"    %  "flexmark-all"       % "0.35.10" % "test"
+    "com.vladsch.flexmark"    %  "flexmark-all"       % "0.62.2"  % "test"
   )
 
   val all: Seq[ModuleID] = compile ++ test
