e604cef34263cf96de978458441942643c5de2f4
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2022-08-09 14:53:10 +0100

    DEP-3637 unused messages and urls removed

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -92,13 +92,7 @@ class AppConfig @Inject()(config: Configuration) {
     "https://www.gov.uk/government/organisations/hm-revenue-customs/contact/vat-registration-applications-exceptions-and-changes"
   val nationalClearanceHubReturnUrl: String =
     "https://www.gov.uk/government/organisations/hm-revenue-customs/contact/national-clearance-hub"
-  val coronavirusGeneralInfoUrl: String = "https://www.gov.uk/coronavirus"
-  val selfEmploymentIncomeSupportReturnUrl: String =
-    "https://www.gov.uk/government/organisations/hm-revenue-customs/contact/get-help-with-the-self-employment-income-support-scheme"
-  val jobRetentionSchemeReturnUrl: String =
-    "https://www.gov.uk/government/organisations/hm-revenue-customs/contact/get-help-with-the-coronavirus-job-retention-scheme"
   val reportFraudulentActivityUrl: String = "https://www.gov.uk/government/organisations/hm-revenue-customs/contact/report-fraud-to-hmrc"
-  val checkCoveredByCoronavirusJRSUrl: String = "https://www.gov.uk/guidance/check-if-you-could-be-covered-by-the-coronavirus-job-retention-scheme"
   val probateReturnUrl: String =
     "https://www.gov.uk/government/organisations/hm-revenue-customs/contact/probate-general-enquiries"
   val c19EmployerEnquiriesReturnRul: String =
