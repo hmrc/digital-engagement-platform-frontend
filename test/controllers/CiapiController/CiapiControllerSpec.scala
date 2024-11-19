36ebd61ebe2562fdef3ec329607d3d2e43f1843a
Andrew <155461893+AndyLearmouth-TDS@users.noreply.github.com>
2024-11-19 14:52:09 +0000

    Adjusted the CiapiControllerSpec.scala to include the new TradeTariff view.

--- test/controllers/CiapiController/CiapiControllerSpec.scala
+++ test/controllers/CiapiController/CiapiControllerSpec.scala
@@ -41,6 +41,7 @@ class CiapiControllerSpec
     mockAuditingService,
     app.injector.instanceOf[AskHMRCOnlineCIAPIView],
     app.injector.instanceOf[NationalMinimumWageCUIView],
+    app.injector.instanceOf[TradeTariffCUIView],
     app.injector.instanceOf[DebtManagementCUIView])
 
   def asDocument(html: String): Document = Jsoup.parse(html)
