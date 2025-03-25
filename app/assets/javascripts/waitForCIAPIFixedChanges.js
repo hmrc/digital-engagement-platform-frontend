05338f410be3872165ac87930774e95daa30ece4
Andrew <155461893+AndyLearmouth-TDS@users.noreply.github.com>
2025-03-25 15:53:53 +0000

    Content: Changed error messages so they are aligned across DAv1, DAv3 and DAv4.

--- app/assets/javascripts/waitForCIAPIFixedChanges.js
+++ app/assets/javascripts/waitForCIAPIFixedChanges.js
@@ -1,30 +1,26 @@
 import * as elementWatcher from './waitForEl'
 import * as statusObserver from './statusObserver'
 import * as dataLayerUpdater from './updateDatalayer'
-import {createDataLayerElement,reportEvent} from './addToDataLayer'
-import {availabilities} from './getAvailability'
+import { createDataLayerElement, reportEvent } from './addToDataLayer'
+import { availabilities } from './getAvailability'
 
 export function waitForCIAPIFixedChanges(w, d) {
   w.addEventListener("load", function () {
-      waitForNuanceElement('#HMRC_CIAPI_Fixed_1',w,d);
+    waitForNuanceElement('#HMRC_CIAPI_Fixed_1', w, d);
   });
 };
 
-function waitForNuanceElement(el,w,d) {
+function waitForNuanceElement(el, w, d) {
   elementWatcher.waitForEl(el + ' div',
-     function () {
-      dataLayerUpdater.updateDataLayer(el,w,d);
-      statusObserver.observeStatus(el,w,d);
+    function () {
+      dataLayerUpdater.updateDataLayer(el, w, d);
+      statusObserver.observeStatus(el, w, d);
     },
-    function() {
-      const assistantUsed = w.location.pathname.includes("webchat")
-        ? "webchat"
-        : "the digital assistant";
-
+    function () {
       var the_element = d.querySelector(el);
-      the_element.textContent = "There's a problem with " + assistantUsed + ". Try again later.";
+      the_element.textContent = "Sorry, there’s a problem with this service. Refresh the page to try again.";
 
-      reportEvent(w,createDataLayerElement(availabilities.NuanceUnavailable, el))
+      reportEvent(w, createDataLayerElement(availabilities.NuanceUnavailable, el))
 
       var elements_to_hide = d.querySelectorAll(".hide-text-on-error");
       for (var i = 0; i < elements_to_hide.length; i++) {
