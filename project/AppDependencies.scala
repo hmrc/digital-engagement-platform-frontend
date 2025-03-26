b62eb368efe2b2e26a995accdf737a00d1ebc93b
dantate92 <122282864+dantate92@users.noreply.github.com>
2025-03-26 16:43:02 +0000

    added ADL banner (#585)
    * added ADL banner
    
    * updated plugins
    
    * updated plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % "9.0.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "10.1.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % "9.11.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "10.13.0",
     "uk.gov.hmrc" %% "digital-engagement-platform-chat-30" % "1.6.0"
   )
 
@@ -17,7 +17,7 @@ object AppDependencies {
     "org.scalatestplus.play" %% "scalatestplus-play" % "6.0.0" % "test",
     "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % "test",
     "org.scalatestplus" %% "mockito-4-6" % "3.2.14.0" % "test",
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" %  "9.0.0" % "test"
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" %  "9.11.0" % "test"
   )
 
   val all: Seq[ModuleID] = compile ++ test
