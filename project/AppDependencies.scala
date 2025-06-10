142750890c38a657dd33c91a6da43417aa4fa9c6
Andrew <155461893+AndyLearmouth-TDS@users.noreply.github.com>
2025-06-10 08:54:40 +0100

    Updated the repo to Scala 3 but need to fix some warnings.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % "9.11.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "10.13.0",
-    "uk.gov.hmrc" %% "digital-engagement-platform-chat-30" % "1.6.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "11.12.0",
+    "uk.gov.hmrc" %% "digital-engagement-platform-chat-30" % "1.7.0"
   )
 
   val test: Seq[ModuleID] = Seq(
