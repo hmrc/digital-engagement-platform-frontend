2e396dc500b4040501560f972b268bdc83b3c255
Andrew Learmouth <155461893+AndyLearmouth-TDS@users.noreply.github.com>
2025-07-17 15:48:18 +0100

    DEP-7147: GOVUK rebrand (#602)
    * Updated play-frontend to 12.3.0 for the gov rebrand.
    
    * DEP-7147 turned useRebrand default in conf file to false
    
    * Updated useRebrand to true.
    
    ---------
    
    Co-authored-by: jacksgannon <19840977+jacksgannon@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "11.13.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.3.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % "9.13.0",
     "uk.gov.hmrc" %% "digital-engagement-platform-chat-30" % "1.7.0"
   )
