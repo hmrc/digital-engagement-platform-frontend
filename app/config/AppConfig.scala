073cf0bdbc15dcdfacbc740360df89db023da860
Andrew <155461893+AndyLearmouth-TDS@users.noreply.github.com>
2025-08-11 13:38:40 +0100

    Created the bereavement DAv4 but needs content and unit tests adding.

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -73,6 +73,7 @@ class AppConfig @Inject()(config: Configuration) {
   val showDAv4DM: Boolean = config.getOptional[Boolean]("features.digitalAssistants.showDAv4DM").getOrElse(false)
   val showDAv4ANH: Boolean = config.getOptional[Boolean]("features.digitalAssistants.showDAv4ANH").getOrElse(false)
   val showDAv4PAYESAR: Boolean = config.getOptional[Boolean]("features.digitalAssistants.showDAv4PAYESAR").getOrElse(false)
+  val showDAv4Bereavement: Boolean = config.getOptional[Boolean]("features.digitalAssistants.showDAv4Bereavement").getOrElse(false)
 
   val testSwitch: Boolean = config.getOptional[Boolean]("features.test").getOrElse(false)
 
