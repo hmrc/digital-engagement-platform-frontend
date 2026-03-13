fb3b0e6238fafcf004ea41e377480b50c222d2ea
Ahmet-Erdogan <118400556+Ahmet-Erdogan@users.noreply.github.com>
2026-03-13 16:37:49 +0000

    new ULR for WMR NI Repayment addded

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -74,6 +74,7 @@ class AppConfig @Inject()(config: Configuration) {
   val showDAv4ANH: Boolean = config.getOptional[Boolean]("features.digitalAssistants.showDAv4ANH").getOrElse(false)
   val showDAv4PAYESAR: Boolean = config.getOptional[Boolean]("features.digitalAssistants.showDAv4PAYESAR").getOrElse(false)
   val showDAv4Bereavement: Boolean = config.getOptional[Boolean]("features.digitalAssistants.showDAv4Bereavement").getOrElse(false)
+  val showDAv4WMRNIRepayment: Boolean = config.getOptional[Boolean]("features.digitalAssistants.showDAv4WMRNIRepayment").getOrElse(false)
 
   val testSwitch: Boolean = config.getOptional[Boolean]("features.test").getOrElse(false)
 
